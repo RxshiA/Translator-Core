a6d689cb7af2d3048039e15325b8eca11b390cad
RxshiA <rashiamarasiri@gmail.com>
2023-09-09 14:07:40 +0530

    Adding styles to the login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/SignupFlow/Login';
 import Register from './components/SignupFlow/Register'
