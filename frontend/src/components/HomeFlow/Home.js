456a30351d2c1b1028f9bc560c481ac44b1008c5
Yasanjith Rajapathirane <125336683+YasanjithR@users.noreply.github.com>
2023-09-10 16:33:07 +0530

    final commit from yasa

--- frontend/src/components/HomeFlow/Home.js
+++ frontend/src/components/HomeFlow/Home.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../../Styles/style.css';
 import Sidebar from '../../views/Sidebar';
 import Navbar from '../../views/NavBar';
 import { dlManelToUnicode, singlishToUnicode, unicodeToDlManel } from "sinhala-unicode-coverter"
+
+let singlishMain = '';
 
 export default class Home extends Component {
   constructor() {
@@ -18,6 +21,8 @@ export default class Home extends Component {
       selectedLanguageTo: 'si-LK',
     };
   }
+
+
 
   componentDidMount() {
     const languages = {
@@ -35,12 +40,14 @@ export default class Home extends Component {
               ? 'selected'
               : ''
             : language_code === 'hi-IN'
-            ? 'selected'
-            : '';
+              ? 'selected'
+              : '';
         let option = `<option ${selected} value="${language_code}">${languages[language_code]}</option>`;
-        tag.insertAdjacentHTML('beforeend', option);
+        // tag.insertAdjacentHTML('beforeend', option); //doubles the selcetcion
       }
     });
+
+
   }
 
   handleInputChange = (event, inputType) => {
@@ -58,11 +65,13 @@ export default class Home extends Component {
       // If input language is Sinhala, convert Singlish to Sinhala
       const sinhalaText = singlishToUnicode(inputValue);
       this.setState({ toText: sinhalaText });
+      singlishMain = this.state.toText
+      //this.setState({fromText:singlishMain})
       return;
     }
 
     // Otherwise, use the original text
-    this.setState({ toText: inputValue });
+    this.setState({ fromText: inputValue });
   };
 
   handleExchangeClick = () => {
@@ -84,23 +93,50 @@ export default class Home extends Component {
     });
   };
 
+
   handleTranslateClick = () => {
-    const { fromText, selectedLanguageFrom, selectedLanguageTo } = this.state;
+    const { fromText, selectedLanguageFrom, selectedLanguageTo, toText } = this.state;
 
     if (!fromText) return;
 
-    const apiUrl = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
-      fromText
-    )}&langpair=${selectedLanguageFrom}|${selectedLanguageTo}`;
 
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        const translatedText =
-          data.responseData.translatedText || data.responseData;
+    if (selectedLanguageFrom === 'si-LK') {
 
-        this.setState({ toText: translatedText });
-      });
+      this.setState({ fromText: singlishMain })
+      const apiUrl = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
+        this.state.toText
+      )}&langpair=${selectedLanguageFrom}|${selectedLanguageTo}`;
+
+      fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          const translatedText =
+            data.responseData.translatedText || data.responseData;
+
+          this.setState({ toText: translatedText });
+        });
+      return;
+    } else {
+
+      const apiUrl = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
+        fromText
+      )}&langpair=${selectedLanguageFrom}|${selectedLanguageTo}`;
+
+      fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          const translatedText =
+            data.responseData.translatedText || data.responseData;
+
+          this.setState({ toText: translatedText });
+        });
+
+
+
+
+    }
+
+
   };
 
   render() {
@@ -114,107 +150,142 @@ export default class Home extends Component {
 
     return (
       <><div className="relative md:ml-64 bg-blueGray-100">
-      <Navbar />
-      <div className="relative bg-pink-600 md:pt-25 pb-5 pt-20"></div>
-    </div>
-      <div className="Home ml-5 mr-0">
-        <div className="container">
-          <div className="wrapper">
-            <div className="text-input">
-              <textarea
-                spellCheck="false"
-                className="from-text"
-                placeholder="Enter text"
-                value={fromText}
-                onChange={(e) => this.handleInputChange(e, 'fromText')}
-              ></textarea>
-              <textarea
-                spellCheck="false"
-                readOnly
-                disabled
-                className="to-text"
-                placeholder="Translation"
-                value={toText}
-              ></textarea>
-            </div>
-            <ul className="controls">
-              <li className="row from">
-                <div className="icons">
-                  <i id="from" className="fas fa-volume-up"></i>
-                  <i className="fas fa-copy"></i>
-                </div>
-                <select
-                  value={selectedLanguageFrom}
-                  onChange={(e) =>
-                    this.setState({ selectedLanguageFrom: e.target.value })
-                  }
-                >
-                  {Object.keys(languages).map((languageCode) => (
-                    <option key={languageCode} value={languageCode}>
-                      {languages[languageCode]}
-                    </option>
-                  ))}
-                </select>
-              </li>
-              <li className="exchange" onClick={this.handleExchangeClick}>
-                <i className="fas fa-exchange-alt"></i>
-              </li>
-              <li className="row to">
-                <select
-                  value={selectedLanguageTo}
-                  onChange={(e) =>
-                    this.setState({ selectedLanguageTo: e.target.value })
-                  }
-                >
-                  {Object.keys(languages).map((languageCode) => (
-                    <option key={languageCode} value={languageCode}>
-                      {languages[languageCode]}
-                    </option>
-                  ))}
-                </select>
-                <div className="icons">
-                  <i id="to" className="fas fa-volume-up"></i>
-                  <i className="fas fa-copy"></i>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <button onClick={this.handleTranslateClick}>Translate Text</button>
-
-
-          <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 bg-white rounded-xl h-full mt-3">
-                    <thead>
-                        <tr>
-                        <th
-                                    scope='col'
-                                    className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase'>
-                                    Input Text
-                                    </th>
-
-                                    <th
-                                    scope='col'
-                                    className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase'>
-                                    Output Text
-                                    </th>
-
-                                 
-                        </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
-                                   Row 1
-                                 </td>
-
-                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
-                                    Row 1
-                                 </td>
-
-
-                    </tbody>
-          </table>
-        </div>
-        <Sidebar />
+        <Navbar />
+        <div className="relative bg-pink-600 md:pt-25 pb-5 pt-20"></div>
       </div>
+        <div className="Home ml-5 mr-0">
+          <div className="container">
+            <div className="wrapper">
+              <div className="text-input">
+                <textarea
+                  spellCheck="false"
+                  className="from-text"
+                  placeholder="Enter text"
+                  value={fromText}
+                  onChange={(e) => this.handleInputChange(e, 'fromText')}
+                ></textarea>
+                <textarea
+                  spellCheck="false"
+                  readOnly
+                  disabled
+                  className="to-text"
+                  placeholder="Translation"
+                  value={toText}
+                ></textarea>
+              </div>
+              <ul className="controls">
+                <li className="row from">
+                  <div className="icons">
+                    <i id="from" className="fas fa-volume-up"></i>
+                    <i className="fas fa-copy"></i>
+                  </div>
+                  <select
+                    value={selectedLanguageFrom}
+                    onChange={(e) =>
+                      this.setState({ selectedLanguageFrom: e.target.value })
+                    }
+                  >
+                    {Object.keys(languages).map((languageCode) => (
+                      <option key={languageCode} value={languageCode}>
+                        {languages[languageCode]}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+                <li className="exchange" onClick={this.handleExchangeClick}>
+                  <i className="fas fa-exchange-alt"></i>
+                </li>
+                <li className="row to">
+                  <select
+                    value={selectedLanguageTo}
+                    onChange={(e) =>
+                      this.setState({ selectedLanguageTo: e.target.value })
+                    }
+                  >
+                    {Object.keys(languages).map((languageCode) => (
+                      <option key={languageCode} value={languageCode}>
+                        {languages[languageCode]}
+                      </option>
+                    ))}
+                  </select>
+                  <div className="icons">
+                    <i id="to" className="fas fa-volume-up"></i>
+                    <i className="fas fa-copy"></i>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <button onClick={this.handleTranslateClick}>Translate Text</button>
+
+
+            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 bg-white rounded-xl h-full mt-3">
+              <thead>
+                <tr>
+                  <th
+                    scope='col'
+                    className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase'>
+                    Input Text
+                  </th>
+
+                  <th
+                    scope='col'
+                    className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase'>
+                    Output Text
+                  </th>
+
+
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                <tr>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    Home
+                  </td>
+
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    නිවසේ
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    අම්මා ගෙදර ගියා
+                  </td>
+
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    Mom went home.
+                  </td>
+                </tr>
+
+                <tr>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    Dog
+                  </td>
+
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    බල්ලා
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    Went home
+                  </td>
+
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600 dark:text-gray-500">
+                    ගෙදර ගියා
+                  </td>
+                </tr>
+
+
+
+              </tbody>
+            </table>
+          </div>
+          <Sidebar />
+        </div>
       </>
     );
   }
